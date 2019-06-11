de1aab746cef3a2c3331423a2461b44c766ca5fb
apwatts <apwatts@chandrakusuma.com>
2019-06-11 12:25:27 +0700

    set to localhost and removed redundent nesting

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def logout():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='127.0.0.1', port=3000)
